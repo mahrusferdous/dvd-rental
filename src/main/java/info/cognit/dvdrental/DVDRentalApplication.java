6b036109160bc7a737dea421e34df947bd5f5673
mahru <mahrusferdous@gmail.com>
2021-06-21 19:06:01 -0400

    service implemented

---
+++ src/main/java/info/cognit/dvdrental/DVDRentalApplication.java
@@ -0,0 +1,13 @@
+package info.cognit.dvdrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DVDRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DVDRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/cognit/dvdrental/FilmRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package info.cognit.dvdrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
